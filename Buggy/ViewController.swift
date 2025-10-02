2d672d6acde57495f9d4eb7e755d707231a9aaf1
TheMAZ2-png <csm012004@outlook.com>
2025-10-02 16:09:13 -0400

    ff

--- Buggy/ViewController.swift
+++ Buggy/ViewController.swift
@@ -11,14 +11,28 @@ class ViewController: UIViewController {
 
     
 @IBAction func buttonTapped(_ sender: UIButton) {
-            print("Called buttonTapped(_:)")
+    print("Method: \(#function) in file: \(#file) line: \(#line) called.")
    }
 
+    
+    func badMethod() {
+        let array = NSMutableArray()
+
+        for i in 0..<10 {
+            array.insert(i, at: i)
+        }
+
+        // Go one step too far emptying the array (notice the range change):
+        for _ in 0...10 {
+            array.removeObject(at: 0)
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        badMethod()
     }
 
-
+  
 }
 
