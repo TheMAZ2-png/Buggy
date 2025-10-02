75c0e5da2a103590dcec1cad8f7651856d304b7c
TheMAZ2-png <csm012004@outlook.com>
2025-10-02 15:31:42 -0400

    Initial Commit

---
+++ BuggyTests/BuggyTests.swift
@@ -0,0 +1,17 @@
+//
+//  BuggyTests.swift
+//  BuggyTests
+//
+//  Created by Chris Mazurek on 10/2/25.
+//
+
+import Testing
+@testable import Buggy
+
+struct BuggyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
