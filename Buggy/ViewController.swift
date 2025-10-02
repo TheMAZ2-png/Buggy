70182154e5b31d5a0647f6e68895efc3213dc4d7
TheMAZ2-png <csm012004@outlook.com>
2025-10-02 15:36:40 -0400

    First

--- Buggy/ViewController.swift
+++ Buggy/ViewController.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        print("Called buttonTapped(_:)")
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
